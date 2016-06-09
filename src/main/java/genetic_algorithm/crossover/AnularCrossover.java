6a62dac3ef229d6f14dd6ff980b219ef925f4890
Nicolas Di Nucci <nico.dinu@gmail.com>
2016-06-09 20:03:08 -0300

    finished crossover

--- src/main/java/genetic_algorithm/crossover/AnularCrossover.java
+++ src/main/java/genetic_algorithm/crossover/AnularCrossover.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 import model.chromosome.CharacterChromosome;
 
-public class OnePointCrossover implements Crossover {
+public class AnularCrossover implements Crossover {
 
 	private Algorithm algorithm;
 	private Mutation mutation;
 
-	public OnePointCrossover() {
+	public AnularCrossover() {
 	}
 	
 	@Override
@@ -31,19 +31,44 @@ public class OnePointCrossover implements Crossover {
 		int chromosomeSize = algorithm.getChromosomes().get(0).getGenes()
 				.size();
 		int locus = (int) (Math.random() * chromosomeSize);
+		int l = (int) (Math.random() * (chromosomeSize/2)+1);
 		List<Double> genesFromCross1 = new ArrayList<>();
 		List<Double> genesFromCross2 = new ArrayList<>();
-		for(int i = 0; i<locus ; i++){
-			genesFromCross1.add(genes1.get(i));
-			genesFromCross2.add(genes2.get(i));			
+		int locusAux = locus + l >= chromosomeSize ?locus+l-chromosomeSize : locus+l;
+		
+		if(locusAux > locus){
+			for(int i = 0; i<locus ; i++){
+				genesFromCross1.add(genes1.get(i));
+				genesFromCross2.add(genes2.get(i));			
+			}
+			for(int i=locus; i<=locusAux; i++){
+				genesFromCross2.add(genes1.get(i));
+				genesFromCross1.add(genes2.get(i));	
+			}
+			for(int i=locusAux+1; i<chromosomeSize; i++){
+				genesFromCross1.add(genes1.get(i));
+				genesFromCross2.add(genes2.get(i));	
+			}
+		} else {
+			for(int i = 0; i<=locusAux ; i++){
+				genesFromCross2.add(genes1.get(i));
+				genesFromCross1.add(genes2.get(i));			
+			}
+			for(int i=locusAux+1; i<locus; i++){
+				genesFromCross1.add(genes1.get(i));
+				genesFromCross2.add(genes2.get(i));	
+			}
+			for(int i=locus; i<chromosomeSize; i++){
+				genesFromCross2.add(genes1.get(i));
+				genesFromCross1.add(genes2.get(i));	
+			}
+			
 		}
-		for(int i=locus; i<chromosomeSize; i++){
-			genesFromCross2.add(genes1.get(i));
-			genesFromCross1.add(genes2.get(i));	
-		}
+		
 		children.add(mutation.mutate(new CharacterChromosome(genesFromCross1)));
 		children.add(mutation.mutate(new CharacterChromosome(genesFromCross2)));
 		return children;
 	}
+
 
 }
--- src/main/java/genetic_algorithm/crossover/UniformCrossover.java
+++ src/main/java/genetic_algorithm/crossover/UniformCrossover.java
@@ -8,18 +8,23 @@ import java.util.List;
 
 import model.chromosome.CharacterChromosome;
 
-public class TwoPointCrossover implements Crossover {
+public class UniformCrossover implements Crossover {
 
 	private Algorithm algorithm;
 	private Mutation mutation;
 	
-	public TwoPointCrossover() {
+	//Chance of a gene to mutate
+	double p;
+	
+	public UniformCrossover(double p){	
+		this.p = p;
 	}
+	
 	
 	@Override
 	public void setAlgorithm(Algorithm algorithm) {
 		this.algorithm = algorithm;
-		this.mutation =algorithm.getMutation();
+		this.mutation = algorithm.getMutation();
 	}
 
 	@Override
@@ -30,28 +35,22 @@ public class TwoPointCrossover implements Crossover {
 		List<CharacterChromosome> children = new ArrayList<>();
 		int chromosomeSize = algorithm.getChromosomes().get(0).getGenes()
 				.size();
-		int locus1 = (int) (Math.random() * chromosomeSize);
-		int locus2 = (int) (Math.random() * chromosomeSize);
-		while(locus1 >= locus2){
-			locus1 = (int) (Math.random() * chromosomeSize);
-			locus2 = (int) (Math.random() * chromosomeSize);
-		}
 		List<Double> genesFromCross1 = new ArrayList<>();
 		List<Double> genesFromCross2 = new ArrayList<>();
-		for(int i = 0; i<locus1 ; i++){
-			genesFromCross1.add(genes1.get(i));
-			genesFromCross2.add(genes2.get(i));			
-		}
-		for(int i=locus1; i<locus2; i++){
-			genesFromCross2.add(genes1.get(i));
-			genesFromCross1.add(genes2.get(i));	
-		}
-		for(int i=locus2; i<chromosomeSize; i++){
-			genesFromCross1.add(genes1.get(i));
-			genesFromCross2.add(genes2.get(i));	
+		for(int i = 0; i<chromosomeSize ; i++){
+			if(Math.random()<p){
+				genesFromCross1.add(genes1.get(i));
+				genesFromCross2.add(genes2.get(i));							
+			} else {
+				genesFromCross2.add(genes1.get(i));
+				genesFromCross1.add(genes2.get(i));	
+			}
+			
 		}
 		children.add(mutation.mutate(new CharacterChromosome(genesFromCross1)));
 		children.add(mutation.mutate(new CharacterChromosome(genesFromCross2)));
 		return children;
+
 	}
+
 }
